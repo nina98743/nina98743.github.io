98e03cbfe8d6c24cab1c9c7586b0198e91857c89
nina98743 <nina.beauce@stcharles-stecroix.org>
2026-02-03 17:37:00 +0100

    Add files via upload

---
+++ docs/contact.php
@@ -0,0 +1,14 @@
+<?php
+ 
+$prenom = $_POST["prénom"];
+$nom = $_POST["nom"];
+$message = $_POST["message"];
+ 
+echo"bonjour ", $prenom ," ", $nom, " le message est envoyé ", "<br>";
+echo"<br>";
+echo $message;
+ 
+ 
+ 
+ 
+?>
